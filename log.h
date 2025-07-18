700895975def05f7c245453bc227f9be2f9bf2f6
Vishesh Meena <visheshmeena47@gmail.com>
2025-07-18 13:42:12 +0530

    Create log.h

---
+++ log.h
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+
+class Console {
+  private:
+    Console() {}
+    Console(const Console&) = delete;
+    Console& operator=(const Console&) = delete;
+  public:
+    static Console& instance() {
+      static Console instance;
+      return instance;
+    }
+    
+    void log(const auto &arg) {
+      std::cout << arg;
+    }
+    void logln(const auto &arg) {
+      std::cout << arg << '\n';
+    }
+    void log(const auto &...Args) {
+      ((std::cout << Args << ' '), ...);
+    }
+    void logln(const auto &...Args) {
+      ((std::cout << Args << ' '), ...);
+      std::cout << '\n';
+    }
+    void log_mln(const auto &...Args) {
+      ((std::cout << Args << '\n'), ...);
+    }
+    
+    // Gives true, if occured successfully.
+    bool get(auto &variable) {
+      bool successful = (std::cin >> variable);
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      return successful;
+    }
+    bool getln(auto &variable) {
+      return std::getline(std::cin, variable);
+    }
+    void getm(auto &...Args) {
+      ((std::cin >> Args), ...);
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    
+    void pause(const std::string &pause_message = "") {
+      std::cout << pause_message;
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    void pause(size_t seconds, const std::string &pause_message = "") {
+      std::cout << pause_message;
+      std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    }
+    void pause_ms(size_t milliseconds, const std::string &pause_message = "") {
+      std::cout << pause_message;
+      std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+    }
+    void pause_ms(float seconds, const std::string &pause_message = "") {
+      std::cout << pause_message;
+      if(seconds < 0) throw std::invalid_argument("Can't sleep for negative seconds.");
+      std::this_thread::sleep_for(std::chrono::milliseconds((size_t)(1000*seconds)));
+    }
+};
