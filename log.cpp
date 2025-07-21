f32eccffbb1cc005d8eaeb76c5561434d1f4e4b6
Vishesh Meena <visheshmeena47@gmail.com>
2025-07-21 18:53:49 +0530

    Update log.cpp

--- log.cpp
+++ log.cpp
@@ -1,24 +1,34 @@
 #include "log.h"
 
-void Console::pause(const std::string &pause_message) {
+Console &console = Console::instance();
+Exception &error = Exception::instance();
+
+//------------------------------CONSOLE----------------------------//
+
+Console& Console::pause(const std::string &pause_message) {
   std::cout << pause_message;
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  return *this;
 }
 
-void Console::pause(size_t seconds, const std::string &pause_message) {
+Console& Console::pause(size_t seconds, const std::string &pause_message) {
   std::cout << pause_message;
   std::this_thread::sleep_for(std::chrono::seconds(seconds));
+  return *this;
 }
 
-void Console::pause(float seconds, const std::string &pause_message) {
+Console& Console::pause(float seconds, const std::string &pause_message) {
   std::cout << pause_message;
   if(seconds < 0) throw std::invalid_argument("Can't sleep for negative seconds.");
   std::this_thread::sleep_for(std::chrono::milliseconds((size_t)(1000*seconds)));
+  return *this;
 }
 
-void Console::pause_ms(size_t milliseconds, const std::string &pause_message) {
+Console& Console::pause_ms(size_t milliseconds, const std::string &pause_message) {
   std::cout << pause_message;
   std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+  return *this;
 }
 
-extern Console &console = Console::instance();
+//---------------------------EXCEPTION----------------------------//
+
