02177d85f591956cbebc8f642aaa9e0b604e02b2
Vishesh Meena <visheshmeena47@gmail.com>
2025-07-21 18:53:25 +0530

    Update log.h

--- log.h
+++ log.h
@@ -11,6 +11,12 @@
 
 // Sugar file....beware of diabetes.
 
+class Console;
+class Exception;
+
+extern Console &console;
+extern Exception &error;
+
 template <typename T>
 requires std::floating_point<T> || std::integral<T>
 std::string operator+(const std::string &str, T value) {return str + std::to_string(value);}
@@ -24,63 +30,137 @@ class Console {
     Console() {}
     Console(const Console&) = delete;
     Console& operator=(const Console&) = delete;
+
   public:
     constexpr static Console& instance() {
       static Console instance;
       return instance;
     }
-    
-    void log(const auto &arg) {std::cout << arg;} // Log to the console.
-    void logln(const auto &arg) {std::cout << arg << '\n';} // Log to the console with a newline character at the end.
-    void log(const auto &...Args) {((std::cout << Args), ...);} // Logs multiple inputs without a space in between.
-    void logln(const auto &...Args) {
+
+    Console& log(const auto &arg) { // Log to the console.
+      std::cout << arg;
+      return *this;
+    }
+    Console& logln() {
+      std::cout << '\n';
+      return *this;
+    }
+    Console& logln(const auto &arg) { // Log to the console with a newline character at the end.
+      std::cout << arg << '\n';
+      return *this;
+    }
+
+    Console& log(const auto &...Args) { // Logs multiple inputs without a space in between.
+      ((std::cout << Args), ...);
+      return *this;
+    }
+    Console& logln(const auto &...Args) {
       ((std::cout << Args), ...);
       std::cout << '\n';
+      return *this;
     }
-    void logs(const auto &...Args) {((std::cout << Args << ' '), ...);}
-    void log_sln(const auto &...Args) {
+
+    Console& logs() {
+      std::cout << ' ';
+      return *this;
+    }
+    Console& logs(const auto &...Args) {
+      ((std::cout << Args << ' '), ...);
+      return *this;
+    }
+    Console& log_sln() {
+      std::cout << " \n";
+      return *this;
+    }
+    Console& log_sln(const auto &...Args) {
       ((std::cout << Args << ' '), ...);
       std::cout << '\n';
+      return *this;
     }
-    void log_mln(const auto &...Args) {((std::cout << Args << '\n'), ...);}
+
+    Console& log_mln(const auto &...Args) {
+      ((std::cout << Args << '\n'), ...);
+      return *this;
+    }
+
     template <typename... Args>
-    void logf(const std::format_string<Args...> &fmt, Args&&... args) {
+    Console& logf(const std::format_string<Args...> &fmt, Args&&... args) {
       std::print(fmt, std::forward<Args>(args)...);
+      return *this;
     }
     template <typename... Args>
-    void log_fln(const std::format_string<Args...> &fmt, Args&&... args) {
+    Console& log_fln(const std::format_string<Args...> &fmt, Args&&... args) {
       std::println(fmt, std::forward<Args>(args)...);
+      return *this;
     }
-    
+
     bool get(auto &variable) { // Gives true, if occured successfully.
       bool successful = (std::cin >> variable);
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       return successful;
     }
-    
-    bool getln(auto &variable) {return std::getline(std::cin, variable);}
-    
+
+    bool getln(auto &variable) {
+      return std::getline(std::cin, variable);
+    }
+
     bool getm(auto &...Args) {
       bool successful = true;
       successful &= (bool)((std::cin >> Args), ...);
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       return successful;
     }
-    
-    void clog(const auto &...Args) {((std::cerr << Args), ...);}
-    
-    void pause(const std::string &pause_message = "");
-    void pause(size_t seconds, const std::string &pause_message = "");
-    void pause(float seconds, const std::string &pause_message = "");
-    void pause_ms(size_t milliseconds, const std::string &pause_message = "");
+
+    Console& clog(const auto &...Args) {
+      ((std::cerr << Args), ...);
+      return *this;
+    }
+
+    Console& pause(const std::string &pause_message = "");
+    Console& pause(size_t seconds, const std::string &pause_message = "");
+    Console& pause(float seconds, const std::string &pause_message = "");
+    Console& pause_ms(size_t milliseconds, const std::string &pause_message = "");
 };
 
-enum class ERR {
+typedef enum class ERR_TYPE {
   runtime_error, invalid_argument, out_of_range, domain_error, logic_error, length_error, bad_alloc, bad_cast, bad_typeid,
-  overflow_error, underflow_error, range_error, ios_failure, custom_error
+  overflow_error, underflow_error, range_error, ios_failure, err_code, custom_error
+} err;
+
+class Error: public std::exception {
+  private:
+    std::string error_message;
+
+  public:
+    Error() {};
+    Error(const std::string &message): error_message(message) {}
+
+    const char* what() const noexcept override {
+      return error_message.c_str();
+    }
 };
 
 class Exception {
   private:
-    Exception() = delete;
+    Exception() {}
+    Exception(const Exception&) = delete;
+    Exception& operator=(const Exception&) = delete;
+
+  public:
+    constexpr static Exception& instance() {
+      static Exception instance;
+      return instance;
+    }
+
+    Exception& raise(err err_type, const auto &message, const std::string &heading = "") noexcept {
+      return *this;
+    }
+    Exception& raise_if(bool if_statement, err err_type, auto &message, const std::string &heading = "") noexcept {
+      return *this;
+    }
+
+    Exception& log(auto &...Args) {
+      ((std::cerr << Args), ...);
+      return *this;
+    }
 };
