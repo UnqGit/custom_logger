0d23ba828d7d1d03d3c5fb8e88f2fcf2b3325f37
Vishesh Meena <visheshmeena47@gmail.com>
2025-07-25 23:44:41 +0530

    Update log.cpp

--- log.cpp
+++ log.cpp
@@ -1,7 +1,7 @@
 #include "log.h"
 
 namespace {
-  std::string get_error_Type(err err_type) noexcept {
+  std::string get_error_type(err err_type) noexcept {
     switch(err_type) {
       case err::runtime_error: return "Runtime-Error";
       case err::invalid_argument: return "Invalid-Argument";
@@ -55,7 +55,7 @@ Console& Console::pause_ms(size_t milliseconds, const std::string &pause_message
 //---------------------------EXCEPTION----------------------------//
 
 Exception& Exception::raise(const std::string &message, err err_type, const std::string &heading, bool throw_err) noexcept {
-  Error error(get_error_Type(err_type) + "\n\t" + heading + "\n\t" + message);
+  Error error(get_error_type(err_type) + "\n\t" + heading + "\n\t" + message);
   if(throw_err) throw error;
   else std::cerr << error.what();
   return *this;
@@ -63,7 +63,7 @@ Exception& Exception::raise(const std::string &message, err err_type, const std:
 
 Exception& Exception::raise_if(bool if_statement, const std::string &message, err err_type, const std::string &heading, bool throw_err) noexcept {
   if(!if_statement) return *this;
-  Error error(get_error_Type(err_type) + "\n\t" + heading + "\n\t" + message);
+  Error error(get_error_type(err_type) + "\n\t" + heading + "\n\t" + message);
   if(throw_err) throw error;
   else std::cerr << error.what();
   return *this;
@@ -71,7 +71,7 @@ Exception& Exception::raise_if(bool if_statement, const std::string &message, er
 
 Exception& Exception::raise_ifn(bool if_statement, const std::string &message, err err_type, const std::string &heading, bool throw_err) noexcept {
   if(if_statement) return *this;
-  Error error(get_error_Type(err_type) + "\n\t" + heading + "\n\t" + message);
+  Error error(get_error_type(err_type) + "\n\t" + heading + "\n\t" + message);
   if(throw_err) throw error;
   else std::cerr << error.what();
   return *this;
